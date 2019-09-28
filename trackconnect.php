fad6770fa0490bd2d45e7cdd47cf22e4a512feef
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-28 09:25:44 +0530

    Update trackconnect.php

--- trackconnect.php
+++ trackconnect.php
@@ -1,18 +1,13 @@
 <?php
 session_start();
-
 $dbservername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "warfortrezor";
 
 $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
-if(isset($_POST['submit']))
-{
-	$answer = $_POST['answer'];
-	$question = $_POST['submit'];
-}
+
     $enrollment = $_SESSION["enrollment"];
-    $sql= "INSERT INTO leaderboard VALUES ('$question','enrollment','$answer');";
+    $sql= "SELECT * FROM responses";
     mysqli_query($conn,$sql);
 ?>
