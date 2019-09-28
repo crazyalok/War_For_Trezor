c29d3c62e4d1ece67b952e6cee3186e17d15fd4c
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-28 09:22:34 +0530

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -1,27 +1,22 @@
 <html>
 <head>
-	<title>Tornado</title>
+	<title>Quiz</title>
   <link rel="icon" href="mil.png" type="image/x-icon">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="asset/css/style.css">
   <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-  <script type="text/javascript" src="asset/javascript/jquery.particleground.js"></script>
-  <script type="text/javascript" src="asset/javascript/demo.js"></script>
-  <link rel="stylesheet" href="asset/css/display.css">
 </head>
 
 <?php include 'leaderboardconnect.php';?>
 
 <body>
-<div id="particles"><canvas class="pg-canvas" width="982" height="754" style="display: block;"></canvas>
-  <div id="intro" style="margin-top: -357.5px;">
     
  <img src="mil.png" align="center" height="120" width="110">
 
   <div class="navbar" align="right">
     <div align="right">
-    <a href="login.php">LOGIN or SIGNUP</a> 
-    <a href="index.php">HOME</a>
+    <a href="question.php">Home</a> 
+    <a href="logout.php">Logout</a>
   </div>
 </div>
 
@@ -31,29 +26,20 @@
   <table class="demo-table" >
     <thead>
       <tr>
-        <th>Name</th>
-        <th>Rank</th>
-        <th>Updated By</th>
-        <th>Updation Date</th>
-        <th>Feats</th>
+        <th>Enrollment</th>
+        <th>Score</th>
       </tr>
     </thead>
     <tbody>
       <?php if ($result = $mysqli->query($query)) { ?>
         <?php while ($row = $result->fetch_assoc()) { ?>
              <?php 
-                    $field1name = $row["name"];
-                    $field2name = $row["rank"];
-                    $field3name = $row["updatedby"];
-                    $field4name = $row["updatedate"];
-                    $field5name = $row["feats"];
+                    $field1name = $row["Enrollment"];
+                    $field2name = $row["Score"];
                      ?>
       <tr>
         <td><?php echo '<b>'.$field1name.'</b>';?></td>
-        <td><?php echo '<b>'.$field2name.'</b>';?></td>
-        <td><?php echo '<b>'.$field3name.'</b>';?></td>
-        <td><?php echo '<b>'.$field4name.'</b>';?></td>
-        <td><?php echo '<b>'.$field5name.'</b>';?></td>  
+        <td><?php echo '<b>'.$field2name.'</b>';?></td> 
       </tr>
 
           <?php } ?>
@@ -63,7 +49,5 @@
   </div>
 </div>
 
-</div>
-</div>
-
-</body></html>
+</body>
+</html>
