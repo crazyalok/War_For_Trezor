680e7013e2693d160237d1851bab655d7c3e90a4
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-28 09:17:27 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,6 +5,10 @@
   <link rel="icon" href="mil.png" type="image/x-icon">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="asset/css/style.css">
+   <script>
+        if(window.history.forward(1) != null)
+        window.history.forward(1);
+   </script>
 </head>
 
 <body>
