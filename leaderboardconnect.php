2e17733d7e447317411e39ba83f046bcca2aff4b
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-28 09:28:18 +0530

    Update leaderboardconnect.php

--- leaderboardconnect.php
+++ leaderboardconnect.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "warfortrezor";
 
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
+
+    $sql= "SELECT enrollment,score FROM users";
+    mysqli_query($conn,$sql);
+?>
