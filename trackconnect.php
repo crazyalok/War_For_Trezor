59e11d2e713afb942b912db38779712a6b97f179
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-28 09:26:19 +0530

    Update trackconnect.php

--- trackconnect.php
+++ trackconnect.php
@@ -7,7 +7,6 @@ $dbname = "warfortrezor";
 
 $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
 
-    $enrollment = $_SESSION["enrollment"];
     $sql= "SELECT * FROM responses";
     mysqli_query($conn,$sql);
 ?>
