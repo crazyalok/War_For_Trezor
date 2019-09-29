72cae7f9d57821147bb5fd953c62783a49ad1255
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-29 18:55:11 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -6,9 +6,10 @@ $_SESSION["enrollment"]=array();
 <head>
   <meta charset="UTF-8">
 	<title>Quiz</title>
-  <link rel="icon" href="mil.png" type="image/x-icon">
+  <link rel="icon" href="https://i.imgur.com/hmaZoKf.gif" type="image/gif">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="asset/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 
 <body>
