3d1e1dd87c5190fedd677c9fcde12834ad1957a1
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-29 18:56:33 +0530

    Update questionconnect.php

--- questionconnect.php
+++ questionconnect.php
@@ -1,32 +1,51 @@
 <?php
-
 session_start();
-
 $dbservername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "warfortrezor";
 
 $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname,3306);
-if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    else
-    {
-    	if(isset($_POST['submit']))
+
+//saving the enrollment number
+$enrollment = $_SESSION["enrollment"];
+    	//it will excute if user try to ascess this page using the url
+		if($_SESSION["enrollment"]==="")
+		{
+				header("location: index.php");
+				exit();
+		}
+		else
+		{
+        //from afterlogin.php
+		if(isset($_POST['submitnew']))
+		{
+          $sql="SELECT * FROM users WHERE enrollment ='$enrollment'";
+		  $result_score= $conn->query($sql);
+		  if ($result_score->num_rows > 0) 
+		  {
+		  $row = $result_score->fetch_assoc();
+	   	  $score = $row['score'];
+		  }
+		  //if answer is wrong then the score as well as quiestion id is not gonna change
+		  $sql="SELECT * from questions WHERE id = '$score'";
+		  $result= $conn->query($sql);
+		  if ($result->num_rows > 0) 
+		  {
+		  $row = $result->fetch_assoc();	
+		  $url=$row['image'];
+		  }
+          header("location: question.php?id=".$url);
+		  $qid=0;
+		  exit();
+        }
+		//from question.php
+    	elseif(isset($_POST['submit']))
 		{
 			$answer = $conn->real_escape_string($_POST['answer']);
 			$qid = $conn->real_escape_string($_POST['qid']);
+            $answer = strtolower($answer);
 		}
-       //it will excute if user try to ascess this page using the url
-		if($_SESSION["enrollment"]==="")
-		{
-				header("location: login.php");
-				exit();
-		}
-				else
-				{
-						$enrollment = $_SESSION["enrollment"];
 		        		$sql="SELECT * FROM users WHERE enrollment ='$enrollment'";
 		        		$result = $conn->query($sql);
 						if ($result->num_rows > 0) 
@@ -70,7 +89,7 @@ if ($conn->connect_error) {
 						$url=$row['image'];
 			        	}
 			        	if($url=="completed")
-			        		{header("location: last.php");}
+			        		{header("location: leaderboard.php");}
 			        	else
 			        	{
 			        	//sending the url using concatation
@@ -106,7 +125,5 @@ if ($conn->connect_error) {
 							}
 						}
 			    }
-				}
-    }
-
+        }
 ?>
