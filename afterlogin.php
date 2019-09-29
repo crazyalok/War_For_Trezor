3badc1b5e0ce873d97a1347874b5ecdfb6e292f0
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2019-09-29 18:52:34 +0530

    Create afterlogin.php

---
+++ afterlogin.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+	<title>Quiz</title>
+  <link rel="icon" href="https://i.imgur.com/hmaZoKf.gif" type="image/gif">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="asset/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script>
+        window.onload = function()
+        {
+          document.getElementById('clickB').click();
+        }
+      </script>
+</head>
+
+<body>
+<form action="./questionconnect.php" method="post">
+<div class="box black">
+<div align="center">
+    <div class="container">
+      <br>
+      <img src="https://i.imgur.com/hmaZoKf.gif">
+      <br>
+      <button type="submit" id="clickB" name="submitnew"><strong></strong></button>
+    </div>
+  </div>
+</div>
+</form>
+</body>
+</html>
